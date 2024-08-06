4c8f98977d746cf39e4b00429e20513f71fd4dbd
Юрий <fedejkin@gmail.com>
2024-08-06 15:09:11 +0300

    Inicial commit

---
+++ src/main/java/sky/pro/calculator_2_12/Calculator212Application.java
@@ -0,0 +1,13 @@
+package sky.pro.calculator_2_12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Calculator212Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Calculator212Application.class, args);
+	}
+
+}
